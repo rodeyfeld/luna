8fc041611f7bc4102d0911dcdb727448946d8485
eric rodefeld <eric.rodefeld@cognitivespace.com>
2024-09-08 00:56:12 -0400

    api integration

--- src/routes/api/imagery/+server.ts
+++ src/routes/api/imagery/+server.ts
@@ -1,26 +1,11 @@
-import { json } from '@sveltejs/kit';
-import type { RequestHandler } from './$types';
-import SwaggerClient from 'swagger-client';
+import type { RequestHandler } from "@sveltejs/kit";
 
-const url = "http://localhost:8000/api/openapi.json"
+export const GET: RequestHandler = async () => {
 
-export const POST: RequestHandler = async ({ request }) => {
-	const { msg} = await request.json();
-	return json(msg + 1);
+  const url = "http://localhost:8000/api/providers";
+	const response = await fetch(url);
+	const data = await response.json();
+	return new Response(JSON.stringify(data));
 };
 
 
-
-// const url = 'https://httpbin.org/post';
-const request = {
-  url, // notice the url must always be part of Request object
-  mode: 'cors',
-  method: 'POST',
-  body: { data: 3 },
-  headers: {
-    'Content-Type': 'application/json',
-  },
-};
-
-const test = await SwaggerClient.http(url, request); // => Promise(Response)
-console.log(test)
